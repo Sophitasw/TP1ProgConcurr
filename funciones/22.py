21aec1844ece6f823742f919242d2598dcd55def
carlos <111547504+carlosjaviercondori@users.noreply.github.com>
2024-04-13 21:32:53 -0300

    Update 22.py

--- funciones/22.py
+++ funciones/22.py
@@ -4,3 +4,4 @@ t2=(t1-int(t1))*24
 t3=(t2-int(t2))*60
 ##print("segundos equivalen a:     ", t1, " Dias     ", t*(1/(60*60))," Horas     ", t*(1/(60))," Minutos     ",t," Segundos")
 print("segundos equivalen a: ",t2)
+hola mundo
